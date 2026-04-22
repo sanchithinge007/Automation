ae92654eadb7255a2c90320ca875df1414511cfe
sanchithinge007 <47769032+sanchithinge007@users.noreply.github.com>
2026-04-22 12:54:09 +0530

    new

--- CucumberFramework/src/test/java/runners/TestRunner.java
+++ CucumberFramework/src/test/java/runners/TestRunner.java
@@ -6,14 +6,23 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-features = "src/test/resources/features",
-glue = {"stepdefinitions", "hooks"},
-plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+    features = "src/test/resources/features",
+    glue = {"stepdefinitions", "hooks"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-report.html",
+        "json:target/cucumber.json",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    },
+    monochrome = true,
+    publish = false
+    // tags = "@smoke or @regression"   // ✅ control execution
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
-	@Override
-	@DataProvider(parallel = true)
-	public Object[][] scenarios() {
-	return super.scenarios();
-	}
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
