ffde5c33bdde3ac5acaaafdf26affbdc0e2c405f
sanchithinge007 <47769032+sanchithinge007@users.noreply.github.com>
2026-04-22 14:03:40 +0530

    new

--- CucumberFramework/src/test/java/hooks/Hooks.java
+++ CucumberFramework/src/test/java/hooks/Hooks.java
@@ -1,8 +1,12 @@
 package hooks;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import driver.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import utils.ConfigReader;
 
 public class Hooks {
@@ -13,9 +17,15 @@ public void setUp() {
 DriverFactory.initDriver(ConfigReader.get("browser"));
 }
 
-
 @After
-public void tearDown() {
-DriverFactory.quitDriver();
+public void tearDown(Scenario scenario) {
+    if (scenario.isFailed()) {
+        byte[] screenshot = ((TakesScreenshot) DriverFactory.getDriver())
+                .getScreenshotAs(OutputType.BYTES);
+
+        scenario.attach(screenshot, "image/png", "Failed Screenshot");
+    }
+
+    DriverFactory.getDriver().quit();
 }
 }
